c45de517b203f8f6a34781dea9677e2afcf6da59
Benjamin <benjaminlivingston@gmail.com>
2016-08-08 16:19:37 -0700

    Initial Commit - C Sharp Drill Exercises beginning with showing differences between accessors.

---
+++ CSharpDrillExercisesPart2/Exercises.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDrillExercisesPart2
+{
+    class Exercises
+    {
+
+        // Code being reused for utility purposes
+        public void clrScreen()
+        {
+            Console.WriteLine("\n\nPress any key to continue . . .");
+            Console.ReadLine();
+            Console.Clear();
+            return;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            
+        }
+    }
+}
